b24a6b96f99f03d9f425b14ba7f2d5fc158913e0
V-Senex <vdm.snx@gmail.com>
2021-07-22 10:57:42 +0300

    Navigation component setup

--- app/src/main/java/com/androidlabentryapp/views/activities/MainActivity.kt
+++ app/src/main/java/com/androidlabentryapp/views/activities/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.androidlabentryapp.views.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
 import com.androidlabentryapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Disabling night-marked themes
+        if (Build.VERSION.SDK_INT >= 29) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        // Setting up navigation host for Jetpack's Navigation component framework
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
     }
 }
