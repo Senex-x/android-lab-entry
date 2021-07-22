9a39e33987198b6fb3923990938c6d69af32bbe8
V-Senex <vdm.snx@gmail.com>
2021-07-22 10:09:46 +0300

    Initial files add

---
+++ app/src/main/java/com/androidlabentryapp/views/fragments/AuthFragment.kt
@@ -0,0 +1,27 @@
+package com.androidlabentryapp.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.androidlabentryapp.R
+
+class AuthFragment : Fragment() {
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_auth, container, false)
+
+        initUi(rootView)
+
+        return rootView
+    }
+
+    private fun initUi(rootView: View) {
+
+    }
+}
---
+++ app/src/main/java/com/androidlabentryapp/views/fragments/RegisterFragment.kt
@@ -0,0 +1,27 @@
+package com.androidlabentryapp.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.androidlabentryapp.R
+
+class RegisterFragment : Fragment() {
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_register, container, false)
+
+        initUi(rootView)
+
+        return rootView
+    }
+
+    private fun initUi(rootView: View) {
+
+    }
+}
